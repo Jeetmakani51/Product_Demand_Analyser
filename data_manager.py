ed73d1083318358d71b5c16b816a6dcec903d0e7
Jeet <makanijeet51@gmail.com>
2026-09-22 21:58:27 +0530

    adding an order function created

--- data_manager.py
+++ data_manager.py
@@ -12,3 +12,27 @@ def load_orders():
     except FileNotFoundError:
         print("File Not Found")
         return pd.DataFrame()
+
+def add_order(Date, Product, Category, Quantity):
+    try:
+        orders = load_orders()
+
+        if orders.empty:
+            Order_ID = 1 #first order
+        else:
+            Order_ID = orders["Order_ID"].max() + 1 #if the max order is 10, the new one order id will be 11
+
+        new_order = {
+            "Order_ID" : Order_ID,
+            "Date" : Date,
+            "Product" : Product,
+            "Category" : Category,
+            "Quantity" : Quantity
+        }
+        orders.loc[len(orders)] = new_order
+        orders.to_csv(FILEPATH, index=False)
+        return True
+
+    except Exception as e:
+        print("Error while adding the order : ", e)
+        return False
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 # print("Retail Product Demand Analyzer")
 # print("Project started successfully!")
 
-from data_manager import load_orders
+from data_manager import load_orders,add_order
 
-orders = load_orders()
+print("before adding an order : \n")
+print(load_orders())
 
-print(orders)
+add_order("22-09-2026", "Biscuit", "Grocery", 5)
+
+print("After adding an order : \n")
+print(load_orders())
